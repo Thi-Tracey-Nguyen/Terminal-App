337a0206343e966f48b0e4aaf42cfbdc21b2f596
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2022-09-16 09:30:44 +0700

    Completed testing for weighted random drawing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# from spellchecker import SpellChecker
 import random
 import itertools
 import string
+from numpy import random as npr
 
 # dictionary = open("dic.txt").read().splitlines()
 
@@ -41,13 +41,13 @@ with open("dic.txt") as word_file:
 
 
 # print(string.ascii_uppercase)
-rack = []
+rack = ['o', 'd', 's', 'a', 'g', 'e']
 
-for i in range(8): 
-    random_letter = random.choice(string.ascii_uppercase)
-    rack.append(random_letter)
+# for i in range(7): 
+#     random_letter = random.choice(string.ascii_uppercase)
+#     rack.append(random_letter)
 
-print(rack)
+# print(rack)
 
 
 # group_number = len(rack)
@@ -58,22 +58,65 @@ print(rack)
 # Test using words in a set
 
 
+
 def shuffle_letters():
     list_words_to_test = []
-    group_number = len(rack)
+    group_number = random.randint(2, 6)
     t = list(itertools.permutations(rack, group_number))
     for i in range(len(t)):
         list_words_to_test.append(''.join(t[i]))
     return list_words_to_test
 
 
+
 def is_english_word():
     for word in shuffle_letters():
-        if word.upper() in english_words: 
+        if word.upper() in english_words:
             return word.upper()
 
+# print(is_english_word())
+
+# Drawing random but based on frequency
+
+tile_bag = {"A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1, "#": 2}
+
+def total_number_of_tiles():
+    """This function calcutes the total number of tiles in the tile bag
+
+    Returns:
+        _int_: total number of tiles
+    """
+    total_number_of_tiles = 0
+    for letter in tile_bag: 
+        frequency = tile_bag[letter]
+        total_number_of_tiles += frequency
+    return total_number_of_tiles 
+
+# print(total_number_of_tiles())
+
+def tile_probability():
+    """This function calculates probabilities of each tile in the bag 
+
+    Returns:
+        _dict_: name of tile and its associated probability
+    """  
+    return {letter : tile_bag[letter] / 100 for letter in tile_bag.keys()}
+
+letter_probabilities = tile_probability()
+
+def deal_tiles(): 
+    """This function randomly deals 5 tiles for each player propotionally to the tile's probalibily
+
+    Returns:
+        __str__: name of tiles
+    """    
+    pass
 
 
+letters = [key for key in letter_probabilities.keys()]
+probabilities = [value for value in letter_probabilities.values()]
+# print(letters)
+# print(probabilities)
 
-shuffle_letters()
-print(is_english_word())
+draw = npr.choice(letters, 5,p=probabilities, replace=False)
+print(draw)
--- test.py
+++ test.py
@@ -1,15 +1,32 @@
-import json
-import gzip
- 
-with gzip.open('/Users/tramnguyenbichtram/Documents/Programming/Projects/term1/python/T1A3/.venv/lib/python3.10/site-packages/spellchecker/resources/en.json.gz', "rb") as f:
-    d = json.loads(f.read().decode("ascii"))
+import random
+import string
+import itertools
 
-print(d)
+with open("dic.txt") as word_file:
+    english_words = set(word.strip() for word in word_file)
 
-  import  jpype     
-  import  asposecells     
-  jpype.startJVM() 
-  from asposecells.api import Workbook
-  workbook = Workbook("input.json")
-  workbook.Save("Output.txt")
-  jpype.shutdownJVM()
+rack = ['d', 'o', 's', 'a', 'g', 'e']
+
+# for i in range(7): 
+#     random_letter = random.choice(string.ascii_uppercase)
+#     rack.append(random_letter)
+
+print(rack)
+
+list_words_to_test = []
+group_number = 5
+t = list(itertools.permutations(rack, group_number))
+for i in range(len(t)):
+  list_words_to_test.append(''.join(t[i]))
+
+# print(t)
+# print(list_words_to_test)
+
+def is_english_word():
+  meaningful_words = []
+  for word in list_words_to_test:
+    if word.upper() in english_words:
+      meaningful_words.append(word.upper())
+  return meaningful_words
+
+print(is_english_word())
