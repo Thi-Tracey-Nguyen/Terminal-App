6a364d77132cc13f2aa051f8908c00cf1f7d6754
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2022-09-23 13:11:11 +0700

    Move files for submission

---
+++ word_game.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+
+if ! [[ -x "$(command -v python3)" ]]
+then
+  echo 'Error: 
+    This program runs on Python, but it looks like Python is not installed.
+    To install Python, check out https://installpython3.com/' >&2
+  exit 1
+fi
+
+python3 -m venv word_game 
+
+cd word_game
+
+source bin/activate
+
+python3 install pip
+
+pip install -r ../requirements.txt
+
+cp ../dic.txt .
+
+python3 ../main.py
